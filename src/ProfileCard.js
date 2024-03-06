b4d57a1f11b3858eee06c6c499fafc048cf23a5e
pjblecha <44296698+pjblecha@users.noreply.github.com>
2024-03-06 16:10:13 -0600

    catchup commit

---
+++ src/ProfileCard.js
@@ -0,0 +1,10 @@
+function ProfileCard({title, handle}) {
+    return (
+        <div>
+            <div>Title: {title}</div>
+            <div>Twitter Handle: {handle}</div>
+        </div>
+    );
+}
+
+export default ProfileCard;
