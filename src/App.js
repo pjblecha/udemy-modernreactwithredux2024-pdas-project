af7dd784a45ade66e32905f7a23847274ba0050e
pjblecha <44296698+pjblecha@users.noreply.github.com>
2024-03-07 14:00:58 -0600

    finished pdas app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -6,10 +7,41 @@ import SiriImage from './images/siri.png';
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cortana" handle="@cortana32" />
-            <ProfileCard title="Sira" handle="@siri01" />
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title="Alexa" 
+                                handle="@alexa99" 
+                                imgSrc={AlexaImage} 
+                                description="Bacon ipsum dolor amet turkey drumstick spare ribs capicola t-bone cupim."
+                            />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title="Cortana" 
+                                handle="@cortana32" 
+                                imgSrc={CortanaImage} 
+                                description="Landjaeger sirloin frankfurter jerky ribeye shankle pork loin alcatra cupim ham hock corned beef shank fatback kevin salami."
+                            />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title="Siri" 
+                                handle="@siri01" 
+                                imgSrc={SiriImage} 
+                                description="Biltong cow pork chop capicola tongue tri-tip pork venison tail turkey kielbasa jowl landjaeger strip steak beef ribs."
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>           
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,18 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({ title, handle, imgSrc, description }) {
     return (
-        <div>
-            <div>Title: {title}</div>
-            <div>Twitter Handle: {handle}</div>
+        <div className="card">
+            <div className="className">
+                <figure className="className">
+                    <img src={imgSrc} alt="pda logo" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">{description}</div>
+            </div>
         </div>
     );
 }
